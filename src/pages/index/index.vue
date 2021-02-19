fdd16a7a67f95092ff7a5b893cd3d337ac6ac9d3
rxfeng <473867601@qq.com>
2021-02-19 10:16:55 +0800

    提交

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,74 +1,74 @@
 <template>
-	<view>
-		<cu-main v-if="view==='Main'"></cu-main>
-		<cu-about v-if="view==='About'"></cu-about>
-		<view class="cu-bar tabbar bg-color3a shadow foot">
-			<view class="action" @click="navChange" data-cur="Main">
-				<view :class="view=='Main'?'cuIcon-homefill text-red':'cuIcon-home text-gray'"></view>
-				<!-- <h1 v-if="view=='Main'">
-					<view class="cu-avatar xs bar-img" style="background-image:url(../../../static/images/l1.png);"></view>
-				</h1>
-				<h1 v-else>
-					<view class="cu-avatar xs bar-img" style="background-image:url(../../../static/images/l2.png);"></view>
-				</h1> -->
-				<view class="text-df" :class="view=='Main'?'text-red':'text-gray'">首页</view>
-			</view>
-			<view class="action" @click="navChange" data-cur="About">
-				<view :class="view=='About'?'cuIcon-myfill text-red':'cuIcon-my text-gray'"></view>
-				<view class="text-df" :class="view=='About'?'text-red':'text-gray'">我的</view>
-			</view>
-		</view>
-	</view>
+    <view>
+        <cu-main v-if="view==='Main'"></cu-main>
+        <cu-about v-if="view==='About'"></cu-about>
+        <view class="cu-bar tabbar bg-color3a shadow foot">
+            <view class="action"
+                  @click="navChange"
+                  data-cur="Main">
+                <view :class="view=='Main'?'cuIcon-homefill text-red':'cuIcon-home text-gray'"></view>
+                <view class="text-df"
+                      :class="view=='Main'?'text-red':'text-gray'">首页</view>
+            </view>
+            <view class="action"
+                  @click="navChange"
+                  data-cur="About">
+                <view :class="view=='About'?'cuIcon-myfill text-red':'cuIcon-my text-gray'"></view>
+                <view class="text-df"
+                      :class="view=='About'?'text-red':'text-gray'">我的123</view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
 import Main from './main'
 import About from './about'
 export default {
-	data() {
-		return {
-			view: 'Main'
-		}
-	},
-	components: { 'cu-main': Main, 'cu-about': About },
-	onLoad() {
-		// ajax
-		// uni.request({
-		// 	url: 'http://localhost:8082/login/userLogin',
-		// 	data: {
-		// 		phone: '15910826618',
-		// 		password: "553311",
-		// 	},
-		// 	method: "POST",
-		// 	header: {
-		// 		'content-type': 'application/x-www-form-urlencoded'
-		// 	},
-		// 	success: function(res){
-		// 		if(res.data.success){
-		//
-		// 		}
-		// 	}
-		// });
-	},
-	methods: {
-		navChange(e) {
-			let toView = e.currentTarget.dataset.cur
-			toView !== this.view ? (this.view = toView) : false
-		}
-	},
-	onShareAppMessage() {
-		return {
-			title: '会跑分享标题'
-		}
-	},
-	onReady() {
+    data() {
+        return {
+            view: 'Main'
+        }
+    },
+    components: { 'cu-main': Main, 'cu-about': About },
+    onLoad() {
+        // ajax
+        // uni.request({
+        // 	url: 'http://localhost:8082/login/userLogin',
+        // 	data: {
+        // 		phone: '15910826618',
+        // 		password: "553311",
+        // 	},
+        // 	method: "POST",
+        // 	header: {
+        // 		'content-type': 'application/x-www-form-urlencoded'
+        // 	},
+        // 	success: function(res){
+        // 		if(res.data.success){
+        //
+        // 		}
+        // 	}
+        // });
+    },
+    methods: {
+        navChange(e) {
+            let toView = e.currentTarget.dataset.cur
+            toView !== this.view ? (this.view = toView) : false
+        }
+    },
+    onShareAppMessage() {
+        return {
+            title: '会跑分享标题'
+        }
+    },
+    onReady() {
 
-	}
+    }
 }
 </script>
 
 <style>
-	.cu-bar .bar-img.xs{
-		margin-left: 0;
-	}
+.cu-bar .bar-img.xs {
+    margin-left: 0;
+}
 </style>
